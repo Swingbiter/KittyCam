66499f7a122f4e7809b0963911a6546461c3bf10
Brady Starling <35944413+Swingbiter@users.noreply.github.com>
2022-05-16 14:58:23 -0500

    added webcam functionality

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
+import cv2
 
 app = Flask(__name__)
 
+# camera stuff
+camera = cv2.VideoCapture(0)
+def gen_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# routes
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -13,6 +28,10 @@ def login():
     else:
         return "frig off"
 
-# fart
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# do the thing
 if __name__ == "__main__":
     app.run(debug=True)
